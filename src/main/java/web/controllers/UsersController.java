2338bb862785e08b09715ca2a84106ef0b3b7c98
rizomus <desmor@mail.ru>
2023-07-10 18:24:03 +0400

    http://localhost:8080/people/ -> http://localhost:8080/users

--- src/main/java/web/controllers/UsersController.java
+++ src/main/java/web/controllers/UsersController.java
@@ -11,7 +11,7 @@ import web.service.PersonServiseInterface;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/people")
+@RequestMapping("/users")
 public class UsersController {
 
     private final PersonServiseInterface userServise;
@@ -24,49 +24,49 @@ public class UsersController {
     @GetMapping()
     public String index(Model model) {
         model.addAttribute("people", userServise.index());
-        return "people/index";
+        return "users/allUsers";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", userServise.show(id));
-        return "people/show";
+        return "users/show";
     }
 
     @GetMapping("/new")
     public String newPerson(@ModelAttribute("person") User user) {
-        return "people/new";
+        return "users/new";
     }
 
     @PostMapping()
     public String create(@ModelAttribute("person") @Valid User user,
                          BindingResult bindingResult) {
         if (bindingResult.hasErrors())
-            return "people/new";
+            return "users/new";
 
         userServise.save(user);
-        return "redirect:/people";
+        return "redirect:/users";
     }
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("person", userServise.show(id));
-        return "people/edit";
+        return "users/edit";
     }
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("person") @Valid User user, BindingResult bindingResult,
                          @PathVariable("id") int id) {
         if (bindingResult.hasErrors())
-            return "people/edit";
+            return "users/edit";
 
         userServise.update(id, user);
-        return "redirect:/people";
+        return "redirect:/users";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
         userServise.delete(id);
-        return "redirect:/people";
+        return "redirect:/users";
     }
 }
