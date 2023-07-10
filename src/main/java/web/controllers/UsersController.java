840f56d2f27af687ef6917107d34da4e19831d78
rizomus <desmor@mail.ru>
2023-07-10 18:32:41 +0400

    model attribute "person" -> "user"

--- src/main/java/web/controllers/UsersController.java
+++ src/main/java/web/controllers/UsersController.java
@@ -22,24 +22,24 @@ public class UsersController {
     }
 
     @GetMapping()
-    public String index(Model model) {
-        model.addAttribute("people", userServise.index());
+    public String allUsers(Model model) {
+        model.addAttribute("users", userServise.index());
         return "users/allUsers";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
-        model.addAttribute("person", userServise.show(id));
+        model.addAttribute("user", userServise.show(id));
         return "users/show";
     }
 
     @GetMapping("/new")
-    public String newPerson(@ModelAttribute("person") User user) {
+    public String newPerson(@ModelAttribute("user") User user) {
         return "users/new";
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") @Valid User user,
+    public String create(@ModelAttribute("user") @Valid User user,
                          BindingResult bindingResult) {
         if (bindingResult.hasErrors())
             return "users/new";
@@ -50,12 +50,12 @@ public class UsersController {
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("person", userServise.show(id));
+        model.addAttribute("user", userServise.show(id));
         return "users/edit";
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") @Valid User user, BindingResult bindingResult,
+    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
                          @PathVariable("id") int id) {
         if (bindingResult.hasErrors())
             return "users/edit";
